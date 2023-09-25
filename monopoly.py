c54ce48685e53232b775a9dd6c0d18f48ffc90e6
thoup99 <thoup99@outlook.com>
2023-09-24 20:04:12 -0400

    Update monopoly.py

--- monopoly.py
+++ monopoly.py
@@ -7,7 +7,7 @@ class Monopoly:
         self.winner = None
 
         self.players = self.getPlayerNames(num_players)
-        self.current_player = 0
+        self.current_player_index = 0
 
         self.num_players = num_players
         self.num_backrupt = 0
@@ -15,15 +15,19 @@ class Monopoly:
         self.dice_1 = Dice(6)
         self.dice_2 = Dice(6)
 
+        self.board = [
+
+        ]
+
     def getPlayerNames(self, num) -> [Player]:
         players = []
         for x in range(num):
             players.append(Player(input(f"Enter player {x + 1}'s name: ")))
 
     def incrementCurrentPlayer(self):
-        self.current_player += 1
-        if self.current_player == self.num_players:
-            self.current_player = 0
+        self.current_player_index += 1
+        if self.current_player_index == self.num_players:
+            self.current_player_index = 0
 
 
     def doOneTurn(self):
@@ -36,9 +40,25 @@ class Monopoly:
                     return
                 
         #If the current player is bankrupt go to the next one
-        while self.players[self.current_player].is_bankrupt == True:
+        current_player = self.players[self.current_player_index]
+        while current_player.is_bankrupt == True:
             self.incrementCurrentPlayer()
+            current_player = self.players[self.current_player_index]
 
-        
-        
+        doubles_rolled = 0
+        loop_again = True
 
+        #Loops while player rolls doubles
+        while loop_again:
+            loop_again = False
+            Dice.roll_all()
+
+            # Handles doubles
+            if self.dice_1 == self.dice_2:
+                loop_again = True
+                doubles_rolled += 1
+
+                #Jails player if they roll 3 doubles in one turn
+                if doubles_rolled == 3:
+                    current_player.is_jailed = True
+                    break
