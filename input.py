9bab619a8674652acb47d7c7b1286b25b9d524fe
thoup99 <thoup99@outlook.com>
2023-11-04 13:07:03 -0400

    Update input.py
    Added an unsubscribe function to the input class

--- input.py
+++ input.py
@@ -9,13 +9,16 @@ class Input:
         pygame.MOUSEBUTTONDOWN: []
     }
 
-    def subscribe(event: pygame.event.Event.type, callback):
-        Input.subscribers[event].append(callback)
-
     def get_inputs():
         for event in pygame.event.get():
             if event.type in Input.subscribers:
                 Input.publish(event)
+
+    def subscribe(event: pygame.event.Event.type, callback):
+        Input.subscribers[event].append(callback)
+
+    def unsubscribe(event: pygame.event.Event.type, callback):
+        Input.subscribers[event].pop(callback)
 
     def publish(event: pygame.event.Event):
         for callback in Input.subscribers[event.type]:
