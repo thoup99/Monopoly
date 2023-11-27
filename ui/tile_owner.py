ab04fa908c0be134bd0a2d2bc5fc98988c1306ab
thoup99 <thoup99@outlook.com>
2023-11-27 10:31:59 -0500

    Adjusted Size of TileOwner

--- ui/tile_owner.py
+++ ui/tile_owner.py
@@ -6,10 +6,10 @@ class TileOwner:
     def __init__(self, ownable_tile: OwnableTile, position) -> None:
         self.tile = ownable_tile
 
-        self.background = ColoredRectangle(position, 24, 24, (0, 0, 0))
+        self.background = ColoredRectangle(position, 19, 19, (0, 0, 0))
 
         if self.tile.owned:
-            self.foreground = ColoredRectangle(position, 20, 20, self.tile.owner.color)
+            self.foreground = ColoredRectangle(position, 15, 15, self.tile.owner.color)
         else:
-            self.foreground = ColoredRectangle(position, 20, 20, (255, 255, 255))
+            self.foreground = ColoredRectangle(position, 15, 15, (255, 255, 255))
 
